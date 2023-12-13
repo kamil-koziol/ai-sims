e4f10548c9de17b170da0897b7846bd18a9906f7
Kacper <kaper02@o2.pl>
2023-12-13 22:33:40 +0100

    zrobienie prostego api do modelu

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from gpt4all import GPT4All
+import os
 
+os.abc
 app = FastAPI()
 
+# tokenizer = AutoTokenizer.from_pretrained("/home/kacper/git/phi-1_5")
+# model = AutoModelForCausalLM.from_pretrained("/home/kacper/git/phi-1_5")
+model = GPT4All("/home/kacper/Downloads/mistral-7b-openorca.Q4_0.gguf")
 
-@app.get('/personas')
-def index():
-    return 'cos'
+
+class GenerationBody(BaseModel):
+    text: str
 
 
+@app.post('/generate')
+async def index(body: GenerationBody):
+    return model.generate(body.text, max_tokens=200)  # [0]['generated_text']
