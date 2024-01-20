e78daba273b20d04320f17ac62167f0413827718
zgooner <kapi77877@gmail.com>
2024-01-20 19:38:56 +0100

    GAME-02: Changed movement feature to different class, added demo for main sample scene

--- game/Assets/Scripts/Agent.cs
+++ game/Assets/Scripts/Agent.cs
@@ -2,21 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 public class Agent : MonoBehaviour {
-    
-    
-    [SerializeField] private Transform currentMovingTarget;
+
+    AgentMovement agentMovement;
     private bool update = true;
-    private NavMeshAgent agent;
 
     private void Awake() {
-        agent = GetComponent<NavMeshAgent>();
-        
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
+
     }
 
     private void Start() {
@@ -42,9 +36,6 @@ public class Agent : MonoBehaviour {
         }
         
         if(!update) return;
-        
-        agent.SetDestination(new Vector3(currentMovingTarget.position.x, currentMovingTarget.position.y, transform.position.z));
+
     }
-    
-    
 }
