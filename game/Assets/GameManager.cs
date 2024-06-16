225b14945481db189d69f82e50b89579f24b0c35
zgooner <kapi77877@gmail.com>
2024-06-16 20:42:38 +0200

    WIP

--- game/Assets/GameManager.cs
+++ game/Assets/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour {
     
     public static GameManager Instance;
     public GameState GameState;
+    private CoroutineQueue coroutineQueue;
+    
     public event Action<GameState> OnGameStateChange;
 
     private void Awake() {
@@ -20,11 +22,28 @@ public class GameManager : MonoBehaviour {
 
     private void Start()
     {
-        BackendService.Instance.SendGameSnapshot();
+        coroutineQueue = new CoroutineQueue(this);
+        coroutineQueue.Enqueue(BackendService.Instance.SendGameSnapshot());
+        foreach (var plan in BackendService.Instance.RequestPlanForAllAgents())
+        {
+            coroutineQueue.Enqueue(plan);
+            //StartCoroutine(plan);
+        }
     }
 
     public void SetGameState(GameState gameState) {
         //GameState = gameState;
         OnGameStateChange?.Invoke(gameState);
+    }
+    
+    public void registerCoroutine(IEnumerator coroutine)
+    {
+        coroutineQueue.Enqueue(coroutine);
+    }
+
+    public Agent[] GetAllAgents()
+    {
+        GameObject agentsGameObj = GameObject.Find("Agents");
+        return agentsGameObj.GetComponentsInChildren<Agent>();
     }
 }
