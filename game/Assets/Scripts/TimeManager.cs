e353930b4172d2182df15674c006a2c9f2ca0fda
Kamil Kozioł <koziol.kamil@protonmail.com>
2024-06-21 13:08:12 +0200

    Added UI clock

--- game/Assets/Scripts/TimeManager.cs
+++ game/Assets/Scripts/TimeManager.cs
@@ -4,28 +4,31 @@ using UnityEngine;
 public class TimeChangedEventArgs : EventArgs
 {
     public DateTime NewTime { get; }
+    public string formattedTime { get; }
 
     public TimeChangedEventArgs(DateTime newTime)
     {
         NewTime = newTime;
+        formattedTime = newTime.ToString("MM/dd/yyyy HH:mm");
     }
 }
 
 public class TimeManager : MonoBehaviour
 {
     private DateTime time;
-    [SerializeField] private DateTime startingDate;
+    private DateTime startingDate;
 
     public delegate void TimeChangedEventHandler(object sender, TimeChangedEventArgs e);
     public event TimeChangedEventHandler OnTimeChanged;
 
-    public const int REALTIME_MULTIPLIER = 60;
+    public const int REALTIME_MULTIPLIER = 60 * 1;
     private float CLOCK_EVERY_S = 1.0f;
 
     private float timer;
 
     void Start()
     {
+        startingDate = new DateTime(2024, 1, 1);
         time = startingDate;
         timer = 0f;
     }
