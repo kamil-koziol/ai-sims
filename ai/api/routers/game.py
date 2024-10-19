5a6d453cbc1d379caa15916dfe8139df38bf30ef
Kamil Kozioł <koziol.kamil@protonmail.com>
2024-10-19 13:05:05 +0200

    Added POST /game/{game_id}/agents endpoint
    Thus refactored old /game/add_agent endpoint

--- ai/api/routers/game.py
+++ ai/api/routers/game.py
@@ -1,7 +1,7 @@
-from typing import Any, Dict, List
+from typing import Annotated, Any, Dict, List
 from uuid import UUID, uuid4
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Path
 from pydantic import BaseModel
 
 from game.game import Game as GlobalGame
@@ -41,27 +41,35 @@ async def create_game(game_request: GameRequest, state: State = Depends(get_stat
     return {"id": game_request.id}
 
 
-class AddAgentRequest(BaseModel):
-    game_id: UUID
+class CreateAgentRequest(BaseModel):
+    name: str
+    age: int
+    description: str
+    lifestyle: str
+
+
+class CreateAgentResponse(BaseModel):
     agent: Agent
 
 
-class AddAgentResponse(BaseModel):
-    game_id: UUID
-
-
-@router.post("/add_agent", response_model=AddAgentResponse)
-async def add_agent(
-    add_agent_request: AddAgentRequest, state: State = Depends(get_state)
+@router.post("/{game_id}/agents", response_model=CreateAgentResponse)
+async def create_agent(
+    game_id: Annotated[UUID, Path(title="Game id")],
+    payload: CreateAgentRequest,
+    state: State = Depends(get_state),
 ):
-    game_id = add_agent_request.game_id
-    agent = add_agent_request.agent
     if game_id not in state.games:
         raise HTTPException(status_code=404, detail="Game not found")
 
-    GameManager().games[game_id].add_agent(
-        AgentMapper.request_to_agent(add_agent_request.agent)
+    agent = Agent(
+        id=uuid4(),
+        name=payload.name,
+        age=payload.age,
+        description=payload.description,
+        lifestyle=payload.lifestyle,
     )
-
     state.games[game_id].agents.append(agent)
-    return {"game_id": game_id}
+
+    GameManager().games[game_id].add_agent(AgentMapper.request_to_agent(agent))
+
+    return CreateAgentResponse(agent=agent)
