c3730bf2594d67c436ac03e6b13a68dc076a696c
Kamil Kozioł <koziol.kamil@protonmail.com>
2024-10-27 14:07:17 +0100

    [#105] Added /games/yaml endpoints

--- ai/api/mappers/game_mapper.py
+++ ai/api/mappers/game_mapper.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-from typing import Dict
+from typing import Dict, List
 from uuid import UUID
 
 from agents.agent import Agent
 from game.game import Game
 
+from ..schemas import Agent as ApiAgent
 from ..schemas import Game as ApiGame
 from .agent_mapper import AgentMapper
 from .location_mapper import LocationMapper
@@ -25,3 +26,15 @@ class GameMapper:
 
         game = Game(game_id=UUID(api_game.id), agents=agents, locations=locations)
         return game
+
+    @staticmethod
+    def game_to_api_game(game: Game) -> ApiGame:
+        agents: List[ApiAgent] = [
+            AgentMapper.agent_to_request(agent) for agent in game._agents.values()
+        ]
+
+        locations = [
+            LocationMapper.location_to_request(location) for location in game._locations
+        ]
+
+        return ApiGame(id=str(game._game_id), agents=agents, locations=locations)
