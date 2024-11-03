c9f08fdc95af4f9c537e903cf582c17f85cd887a
Kamil Kozioł <koziol.kamil@protonmail.com>
2024-11-03 21:37:18 +0100

    quickfix MOCK_MODELS

--- ai/config/model.py
+++ ai/config/model.py
@@ -1,5 +1,5 @@
 import os
 
-MOCK_MODELS = True if os.environ["MOCK_MODELS"].lower() == "true" else False
+MOCK_MODELS = (os.environ.get("MOCK_MODELS") or "").lower() == "true"
 EMBEDDING_URL = "http://localhost:8888/embed"
 GENERATION_URL = "http://localhost:8888/generate"
