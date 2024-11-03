0c09d013e1a38fa37a7b538c68bb389c961c6598
zgooner <kapi77877@gmail.com>
2024-11-03 15:38:03 +0100

    Added very cool UI

---
+++ game/Assets/GUIAgentComplexMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class GUIAgentComplexMenu : MonoBehaviour
+    {
+        public GameObject visual;
+        public GameObject characterItemPrefab;
+        public Transform contentPanel;
+        public Button submitButton;
+        public TMP_InputField textField;
+        public Toggle checkbox;
+        
+        private global::Agent selectedAgent = null;
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                GameManager.Instance.SetGameState(GameState.WAITING_FOR_RESULTS);
+                foreach (global::Agent ag in GameManager.Instance.GetAgents())
+                {
+                    GameObject go = Instantiate(characterItemPrefab, contentPanel);
+                    Button charButton = go.GetComponentInChildren<Button>();
+                    TMP_Text nameText = charButton.GetComponentInChildren<TMP_Text>();
+                    global::Agent.AgentState state = ag.getAgentState();
+                    nameText.text = state.agentName;
+                    charButton.onClick.AddListener(() => OnCharacterSelected(ag));
+                }
+                visual.SetActive(true);
+            }
+        }
+        
+        void OnCharacterSelected(global::Agent ag)
+        {
+            Debug.Log("Selected character: " + ag.getAgentState().agentName);
+            selectedAgent = ag;
+        }
+    }
+}
